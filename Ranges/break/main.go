8e4a4d10859052c45b6d175c03a19378aedac7b1
muhammad sohail <sohail20338@gmail.com>
2026-06-10 20:43:22 +0500

    practicing

--- Ranges/break/main.go
+++ Ranges/break/main.go
@@ -3,13 +3,15 @@ package main
 import "fmt"
 
 func main() {
+	for i := 1; i <= 20; i++ {
 
-	for i := 1; i <= 10; i++ {
+		if i == 9 {
+			fmt.Println("9 is my favorite number")
+			break
 
-		if i == 4 {
-			break // stop the loop
 		}
+		fmt.Println("here is the number")
 		fmt.Println(i)
 	}
-
+	fmt.Println("Exit the loop")
 }
