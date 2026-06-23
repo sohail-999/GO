e7ac7fdb316b849887262889e6f221564de23b6d
muhammad sohail <sohail20338@gmail.com>
2026-06-23 15:37:55 +0500

    changing types

---
+++ Generic_/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+)
+
+func printslice[T any](items []T) {
+
+	for _, item := range items {
+
+		fmt.Println(item)
+
+	}
+
+}
+func main() {
+
+	names := []bool{ //here we specify the type
+		true, false}
+
+	printslice(names)
+
+}
+
+/*type vertex struct {
+	X float64
+
+	Y float64
+}
+
+func (v vertex) abs() float64 {
+
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+
+}
+func main() {
+
+	v := vertex{
+		5, 6,
+	}
+
+	fmt.Println(v.abs())
+
+}
+*/
