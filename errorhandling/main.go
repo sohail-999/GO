49839204cd83edfca30db1c32285de2a842ba2b9
muhammad sohail <sohail20338@gmail.com>
2026-06-24 18:14:55 +0500

    added comments

--- errorhandling/main.go
+++ errorhandling/main.go
@@ -6,11 +6,11 @@ import (
 )
 
 func main() {
-	file, err := os.Open("Data.txt")
+	file, err := os.Open("Daata.txt") //the os allows to interact with opearting system
 
 	if err != nil {
 		fmt.Println("Error:", err) //if the file does not exists its gonna print
-		return
+
 	}
 
 	fmt.Println("File opened successfully:", file.Name()) //if the file exists in the folder it this gonna print
