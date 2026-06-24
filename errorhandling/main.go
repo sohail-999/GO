42855d43b638a627895a4f2413ee21a99b39e4b0
muhammad sohail <sohail20338@gmail.com>
2026-06-24 23:07:12 +0500

    organinzing

--- errorhandling/main.go
+++ errorhandling/main.go
@@ -6,12 +6,27 @@ import (
 )
 
 func main() {
-	file, err := os.Open("Data.txt") //the os allows to interact with opearting system
+
+	// @ for opening the file
+	file, err := os.Open("Daata.txt") //the os allows to interact with opearting system
 
 	if err != nil {
-		fmt.Println("Error:", err) //if the file does not exists its gonna print
+		fmt.Println("Error :", err) //fmt.Println("Error:", err) //if the file does not exists its gonna print
+
 		return
 	}
 
 	fmt.Println("File opened successfully:", file.Name()) //if the file exists in the folder it this gonna print
+
+	//@ for reading the file
+
+	// 	file,err :=os.ReadFile("Data.txt")
+
+	// 	if err!=nil{
+
+	//	      fmt.Println("Error:", err)
+	//	      return
+	//		}
+	//
+	// fmt.Println("File readed suucessfully:",string(file))
 }
