7ae4810c4c223e2ee8cd810cd57c48f9833ee95b
muhammad sohail <sohail20338@gmail.com>
2026-06-26 05:13:25 +0500

    ...

--- Type interference/main.go
+++ Type interference/main.go
@@ -19,8 +19,8 @@ func main() {
 	fmt.Println(Add(10, 20))     // Go infers T = int
 	fmt.Println(Add(3.14, 2.86)) // Go infers T = float64
 
-	fmt.Println(Multiply(4, 5))     // Go infers T = int
-	fmt.Println(Multiply(2.5, 4.0)) // Go infers T = float64
+	fmt.Println(Multiply(4, 5))       // Go infers T = int
+	fmt.Println(Multiply(2.111, 4.0)) // Go infers T = float64
 
 	Print("Type Inference") // Go infers T = string
 	Print(100)              // Go infers T = int
