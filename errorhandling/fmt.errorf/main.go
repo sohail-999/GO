2a2e146340909092e4c10ab85eb90a1d6e47ba7d
muhammad sohail <sohail20338@gmail.com>
2026-06-29 20:25:27 +0500

    ...

---
+++ errorhandling/fmt.errorf/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	err := fmt.Errorf("the error occured %v :", time.Now())
+	fmt.Println("the error happened at the :", err)
+
+	//const name, depart = "geekforgeeks", "CS"
+	//err := fmt.Errorf("%q is a %q portal", name, depart)
+
+	//fmt.Println(err.Error())
+
+}
