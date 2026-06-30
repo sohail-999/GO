d66b2909db762cb7a6a90c51b3314e55469bd55d
muhammad sohail <sohail20338@gmail.com>
2026-06-30 22:14:56 +0500

    defer...

---
+++ errorhandling/defer/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func Copyfile(srcName, dstName string) (written int64, err error) {
+	src, err := os.Open(srcName)
+	if err != nil {
+		return
+
+	}
+	defer src.Close()
+	dst, err := os.Create(dstName)
+	if err != nil {
+		return
+	}
+
+	defer dst.Close()
+	return io.Copy(dst, src)
+
+}
+func main() {
+
+	written, err := Copyfile("source.txt", "destination.txt")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Println("Bytes written:", written)
+}
