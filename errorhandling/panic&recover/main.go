f740f2bab2603ec2cf996b29a0f747dd5924ae37
muhammad sohail <sohail20338@gmail.com>
2026-07-01 19:42:14 +0500

    panicss...

--- errorhandling/panic&recover/main.go
+++ errorhandling/panic&recover/main.go
@@ -4,9 +4,18 @@ import (
 	"fmt"
 )
 
+type Shark struct {
+	Name string
+}
+
+func (s *Shark) Swim() {
+	fmt.Println(s.Name, "is swimming")
+}
+
 func main() {
-	arr := []int{1, 2, 3, 4, 5} //array of 5 integers
-
-	fmt.Println("Array:", arr[len(arr)]) //length of the array
-
+	//arr := []int{1, 2, 3, 4, 5} //array of 5 integers
+	//fmt.Println("Array:", arr[len(arr)]) //length of the array
+	s := &Shark{"sammy"}
+	s = nil
+	s.Swim() //invoking the method
 }
