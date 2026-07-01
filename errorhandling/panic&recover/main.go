75a5cbd1554e5f3103ed3cf2b3fd097e1693d8b5
muhammad sohail <sohail20338@gmail.com>
2026-07-01 19:49:14 +0500

    pointer to nothing

--- errorhandling/panic&recover/main.go
+++ errorhandling/panic&recover/main.go
@@ -15,7 +15,7 @@ func (s *Shark) Swim() {
 func main() {
 	//arr := []int{1, 2, 3, 4, 5} //array of 5 integers
 	//fmt.Println("Array:", arr[len(arr)]) //length of the array
-	s := &Shark{"sammy"}
-	s = nil
-	s.Swim() //invoking the method
+	s := &Shark{Name: "sammy"} //pointer to the shark
+	s = nil                    //pointer to nothing
+	s.Swim()
 }
