e1a7b4db2f230fb5218bb87f6029fa762053162d
muhammad sohail <sohail20338@gmail.com>
2026-07-06 17:05:12 +0500

    errorwrapping&unwrapping

--- errorhandling/fmt.errorf/main.go
+++ errorhandling/fmt.errorf/main.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
 func main() {
 
+	//@nested error handling for printing the time of the error
+
 	//err := fmt.Errorf("the error occured %v :", time.Now())
 	//fmt.Println("the error happened at the :", err)
 
-	const name, depart = "geekforgeeks", "CS"
-	err := fmt.Errorf("%q is a %q portal", name, depart)
+	//@ printing the error using %q(which is used for putting the quotes)
 
-	fmt.Println(err.Error())
+	//const name, depart = "geekforgeeks", "CS"
+
+	//err := fmt.Errorf("%q is a %q portal", name, depart)
+
+	//fmt.Println(err.Error())
+	originalerror := errors.New("original error")
+	wrappederror := fmt.Errorf("additional information %w", originalerror)
+	fmt.Println(wrappederror)
+	//if errors.Is(wrappederror, originalerror) {
+	//	fmt.Println("the wrapped contain the original...")
+
+	// }
+	unwrappederror := errors.Unwrap(wrappederror)
+	fmt.Println(unwrappederror)
 
 }
