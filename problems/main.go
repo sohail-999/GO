6321f7496b24826be3ae6f6f2b7af24fd4993724
muhammad sohail <sohail20338@gmail.com>
2026-07-13 22:32:00 +0500

    problem

---
+++ problems/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Foo struct {
+	firstDone  chan struct{}
+	secondDone chan struct{}
+}
+
+func NewFoo() *Foo {
+	return &Foo{
+		firstDone:  make(chan struct{}),
+		secondDone: make(chan struct{}),
+	}
+}
+
+func (f *Foo) First(printFirst func()) {
+	// Do not change this line
+	printFirst()
+
+	close(f.firstDone)
+}
+
+func (f *Foo) Second(printSecond func()) {
+	<-f.firstDone
+
+	// Do not change this line
+	printSecond()
+
+	close(f.secondDone)
+}
+
+func (f *Foo) Third(printThird func()) {
+	<-f.secondDone
+
+	// Do not change this line
+	printThird()
+}
+
+func main() {
+	foo := NewFoo()
+
+	var wg sync.WaitGroup
+	wg.Add(3)
+
+	go func() {
+		defer wg.Done()
+		foo.First(func() {
+			fmt.Print("first")
+		})
+	}()
+
+	go func() {
+		defer wg.Done()
+		foo.Second(func() {
+			fmt.Print("second")
+		})
+	}()
+
+	go func() {
+		defer wg.Done()
+		foo.Third(func() {
+			fmt.Print("third")
+		})
+	}()
+
+	wg.Wait()
+}
