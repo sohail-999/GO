5345ec356ac7236d6f732fcb762a9afa578e875f
muhammad sohail <sohail20338@gmail.com>
2026-07-16 21:53:06 +0500

    cli

---
+++ CLI/urfavecli/go.mod
@@ -0,0 +1,11 @@
+module urfave-cli
+
+go 1.23.0
+
+require github.com/urfave/cli v1.22.17
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.7 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/stretchr/testify v1.11.1 // indirect
+)
---
+++ CLI/urfavecli/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+
+	app.Name = "My New CLI Application"
+	app.Action = (func(ctx *cli.Context) error {
+		fmt.Println("app launched")
+		return nil
+
+	})
+	err := app.Run(os.Args)
+	if err != nil {
+
+		log.Fatal(err)
+
+	}
+
+}
