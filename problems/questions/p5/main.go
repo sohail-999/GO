259bc6b27e4875b9b8e201b918b206b7cddde81e
muhammad sohail <sohail20338@gmail.com>
2026-07-18 22:14:33 +0500

    comments

--- problems/questions/p5/main.go
+++ problems/questions/p5/main.go
@@ -1,8 +1,8 @@
 // @write a program which reads the length and breadth of a rectangle
 // and then prints area and perimter
 // reads the length and breadth
-// apply the area  formula
-// apply the perimeter formula
+// apply the area  formula ,area=length * width
+// apply the perimeter formula,perimter=2(lenght +  width)
 package main
 
 import "fmt"
