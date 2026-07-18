17603dac66a24b05f62b48cbc789081047aefee1
muhammad sohail <sohail20338@gmail.com>
2026-07-18 22:12:49 +0500

    ...

--- problems/questions/p5/main.go
+++ problems/questions/p5/main.go
@@ -1,10 +1,15 @@
+// @write a program which reads the length and breadth of a rectangle
+// and then prints area and perimter
+// reads the length and breadth
+// apply the area  formula
+// apply the perimeter formula
 package main
 
 import "fmt"
 
-func Areaandperimeter(length int, breadth int) {
-	fmt.Println("area :", length*breadth)
-	fmt.Println("Perimeter:", 2*(length+breadth))
+func Areaandperimeter(l int, b int) {
+	fmt.Println("area:\n", l*b)
+	fmt.Println("Perimeter:\n", 2*(l+b))
 
 }
 func main() {
