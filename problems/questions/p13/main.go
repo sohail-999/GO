f6f5d3ab8d9138eb7b81b11739bce9ef22324f08
muhammad sohail <sohail20338@gmail.com>
2026-07-28 18:13:11 +0500

    p13

---
+++ problems/questions/p13/main.go
@@ -0,0 +1,23 @@
+// Write a program to read the three numbers and then find the smallest among them
+// first get the three  numbers from the users as the input
+// apply the some condition to to find the smallest number
+package main
+
+import "fmt"
+
+func main() {
+
+	var a, b, c int
+	fmt.Println("Please enter the number:")
+
+	fmt.Scan(&a, &b, &c)
+
+	smallest := a
+	if b < smallest {
+		smallest = b
+	}
+	if c < smallest {
+		smallest = c
+	}
+	fmt.Println("Smallest number:", smallest)
+}
