c70c635836706001dc5020c6988895ffc9128edf
muhammad sohail <sohail20338@gmail.com>
2026-07-28 22:38:49 +0500

    p14_updated

---
+++ problems/questions/p14/main.go
@@ -0,0 +1,27 @@
+//Write a program that  reads a year and check whether  its leap year or not
+//first get the year as input from the user
+//then apply the some condition
+
+package main
+
+import "fmt"
+
+func main() {
+
+	var year int
+	fmt.Println("Please enter the number:")
+	fmt.Scan(&year)
+	leapyear := year
+
+	if leapyear%4 == 0 {
+		leapyear = year
+		//fmt.Printf("%v is a leap year\n", year)
+
+	}
+	if leapyear%4 == 0 && leapyear%100 == 0 && leapyear%400 == 0 {
+		leapyear = year
+		//fmt.Printf("%v is leap year\n", year)
+	}
+	fmt.Printf("YES , %v is a leapyear\n", leapyear)
+
+}
