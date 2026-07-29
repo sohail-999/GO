8f2ffdeed60bd35d2c1010882011a15d3ea829c0
muhammad sohail <sohail20338@gmail.com>
2026-07-29 17:01:50 +0500

    p14_updated

--- problems/questions/p14/main.go
+++ problems/questions/p14/main.go
@@ -1,7 +1,7 @@
-//Write a program that  reads a year and check whether  its leap year or not
-//first get the year as input from the user
-//then apply the some condition
-
+// Write a program that  reads a year and check whether  its leap year or not
+// first get the year as input from the user
+// then apply the some condition
+// leap year is divsible by 4 or divisible by 4,100 and 400 otherwise its not leap year
 package main
 
 import "fmt"
@@ -14,13 +14,21 @@ func main() {
 	leapyear := year
 
 	if leapyear%4 == 0 && leapyear%100 != 0 {
-		fmt.Println("this is a leap year")
+		fmt.Printf("%v is a leap year\n", leapyear)
 		//fmt.Printf("%v is a leap year\n", year)
 
+	}
+	if leapyear%4 != 0 {
+		fmt.Printf("%v is not a leap year\n", leapyear)
 	}
 	if leapyear%4 == 0 && leapyear%100 == 0 && leapyear%400 == 0 {
 		leapyear = year
 		fmt.Printf("YES , %v is a leapyear\n", leapyear)
+		//fmt.Printf("%v is leap year\n", year)
+	}
+	if leapyear%4 == 0 && leapyear%100 == 0 && leapyear%400 != 0 {
+		leapyear = year
+		fmt.Printf("NO , %v is not a leapyear\n", leapyear)
 		//fmt.Printf("%v is leap year\n", year)
 	}
 
